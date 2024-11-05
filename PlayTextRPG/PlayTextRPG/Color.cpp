599e7c90966b57edfaa0f337e3092985ee52529a
Yugeyong <69104743+YUgyeong-Choi@users.noreply.github.com>
2024-11-05 13:36:59 +0900

    PlayTextRPG파일 삭제

--- PlayTextRPG/PlayTextRPG/Color.cpp
+++
@@ -1,7 +0,0 @@
-#include "Color.h"
-#include <windows.h> 
-
-void setColor(unsigned short text) {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    SetConsoleTextAttribute(hConsole, text);
-}
